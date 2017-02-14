cd47f8bcb1cf0ecb08c6a3f0c3f7a441e99e3a81
hdoedens <hdoedens@gmail.com>
2017-02-14 22:11:59 +0100

    added autoload code

--- api/index.php
+++ api/index.php
@@ -10,6 +10,9 @@ if (PHP_SAPI == 'cli-server') {
 }
 
 require __DIR__ . '/../vendor/autoload.php';
+spl_autoload_register(function ($classname) {
+    require ("../classes/" . $classname . ".php");
+});
 
 session_start();
 
