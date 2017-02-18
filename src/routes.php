7325de4cd62e8a9ac65c4e9877d50d5d0908a8d4
hdoedens <hdoedens@gmail.com>
2017-02-18 14:33:05 +0100

    added tutorial route

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,16 @@
 <?php
 // Routes
 
+$app->get('/tickets', function ($request, $response, $args) {
+    $this->logger->info("Ticket list");
+    // $mapper = new TicketMapper($this->db);
+    // $tickets = $mapper->getTickets();
+
+    // $response->getBody()->write(var_export($tickets, true));
+    // return $response;
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
 $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
